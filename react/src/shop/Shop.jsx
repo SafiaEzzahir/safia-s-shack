1af225a04c13c8e34978d0f3abd93741ebcbbb69
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-12-22 10:36:07 +0000

    Make layout for shop page

--- react/src/shop/Shop.jsx
+++ react/src/shop/Shop.jsx
@@ -1,7 +1,28 @@
+import './Shop.css'
+
+// if clicked on shop button - open right section
+// else - hide right section
+
+function RightSection(){
+    // side navbar with icons that expand into names on hover? idk yet
+    return(
+        <div id="RightSection">
+            <ul>
+                <li>this</li>
+                <li>is</li>
+                <li>the</li>
+                <li>right</li>
+                <li>section</li>
+            </ul>
+        </div>
+    )
+}
+
 function Shop(){
     return(
-        <div>
+        <div id="shop">
             <p>hello this is the shop</p>
+            <RightSection />
         </div>
     )
 }
