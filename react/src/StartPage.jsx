d7a1a0f04cde00b9843b65ec543d043679d412ed
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-12-23 15:39:58 +0000

    I feel like i havent been committing every hour

---
+++ react/src/StartPage.jsx
@@ -0,0 +1,14 @@
+import './StartPage.css'
+
+function StartPage() {
+    return (
+        <div id='StartPage'>
+            <p>safia ezzahir</p>
+            <p>a personal website inspired by text-adventure video games</p>
+            <button>start</button>
+            <button>click here to skip to my portfolio</button>
+        </div>
+    )
+}
+
+export default StartPage;
