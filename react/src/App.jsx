c0aa5c977f268c08f6a49d1add45d085558c1437
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-12-23 15:40:33 +0000

    Oops forgot to add these two

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,22 +3,46 @@ import { useEffect } from 'react'
 import './App.css'
 import Shop from './shop/Shop.jsx'
 import SpeechWindow from './GameStuff.jsx'
+import StartPage from './StartPage.jsx'
 
 function App() {
-  const [currentStage, setCurrentStage] = useState('intro')
+  const [currentStage, setCurrentStage] = useState('');
 
-  // check what stage you're on - intro or shop
-  if (currentStage === 'intro'){
+  useEffect(() => {
+    if (sessionStorage.getItem("currentStage")) {
+      console.log("currentstage exists in storage");
+      setCurrentStage(sessionStorage.getItem("currentStage"));
+    } else {
+      console.log("it doesnt exist");
+      setCurrentStage('start');
+      sessionStorage.setItem("currentStage", "start")
+  }}, []);
+
+  // check what stage you're on - start, intro, or shop
+  
+  if (currentStage === 'start') {
+
     return (
+      <StartPage />
+    );
+
+  } else if (currentStage === 'intro'){
+
+    return (
+
       <div className='container'>
         <img src="src/assets/safia3.png" alt="safia" />
-        <SpeechWindow current={currentStage} onFinishLevels={() => setCurrentStage('shop')} />
+        <SpeechWindow current={currentStage} onFinishLevels={() => {setCurrentStage('shop'); sessionStorage.setItem("currentStage", "shop")}} />
       </div>
+
     );
+
   } else if (currentStage === 'shop') {
+    
     return(
       <Shop />
     );
+
   };
 }
 
