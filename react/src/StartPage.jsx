a4f544a2673ac11c128bd8b5b3665feb9c1c9e6a
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-12-23 17:43:35 +0000

    Improve StartPage title

--- react/src/StartPage.jsx
+++ react/src/StartPage.jsx
@@ -3,8 +3,8 @@ import './StartPage.css'
 function StartPage({ onStartClick, onSkipClick }) {
     return (
         <div id='StartPage'>
-            <p>safia ezzahir</p>
-            <p>a personal website inspired by text-adventure video games</p>
+            <p id='title'>safia ezzahir</p>
+            <p id="desc">a personal website inspired by text-adventure video games</p>
             <button onClick={onStartClick}>start</button>
             <button onClick={onSkipClick}>click here to skip to my portfolio</button>
         </div>
