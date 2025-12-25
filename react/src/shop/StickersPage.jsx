230858695e9348d4d3ff88789979f46d5cf08dc4
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-12-25 10:01:31 +0000

    Forgot to commit this as part of StickersPage

--- react/src/shop/StickersPage.jsx
+++ react/src/shop/StickersPage.jsx
@@ -1,9 +1,38 @@
+import { useEffect } from 'react';
+
 import './ShopPage.css'
+
+// iterate through json file/ !folder!
+// add div with title:desc + img from stickers folder in public
+
+// for now, add them manually
+
+//
+
+const images = ['crying orpheus', 'cute lightbulb', 'girls who code clicker']
+
+//var ImagesToUse = {}
+
+//for (var image in images) {
+//    image = images[image]
+//    image = './' + image;
+//    image += '.jpg';
+//    ImagesToUse.push(image);
+//}
 
 function StickersPage() {
     return (
-        <div>
-            <p>this is the sticker page</p>
+        <div className='ItemsPage'>
+            <h1>STICKERS</h1>
+            <img src="./react/public/stickers/crying orpheus.jpg" alt="yhooohoo" />
+            <div className='ItemsSection'>
+                {images.map((image) => (
+                    <div className='Item'>
+                        <img className='ItemImage' src={'./' + image + '.jpg'} alt={'a ' + image + ' sticker'} />
+                        <p className='ItemText'>{image}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
