5a563edec6457ae72a376b5f53d3541fbf4f1cc6
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-12-25 17:40:04 +0000

    Create PlantsPage

--- react/src/shop/PlantsPage.jsx
+++ react/src/shop/PlantsPage.jsx
@@ -1,6 +1,26 @@
+import './ShopPage.css'
+
+const imagesModules = import.meta.glob('../assets/stickers/*.{png,jpg,jpeg,gif}', { eager: true, as: 'url' });
+const imagesList = Object.entries(imagesModules).map(([path, url]) => {
+    const parts = path.split('/');
+    const filename = parts[parts.length - 1];
+    const name = filename.replace(/\.(png|jpg|jpeg|gif)$/i, '');
+    return { name, url };
+});
+
 function PlantsPage() {
     return (
-        <p>plants</p>
+        <div className='ItemsPage'>
+            <h1 className='ItemsText'>PLANTS</h1>
+            <div className='ItemsSection'>
+                {imagesList.map((image) => (
+                    <div className='Item'>
+                        <img className='ItemImage' src={image.url} alt={'a ' + image.name + ' sticker'} />
+                        <p className='ItemText'>{image.name}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
