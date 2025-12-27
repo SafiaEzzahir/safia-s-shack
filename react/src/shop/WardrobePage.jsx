f2e2818a1db592210e10850fec3a8391b935fe79
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-12-27 10:59:14 +0000

    Open Clothes on click

--- react/src/shop/WardrobePage.jsx
+++ react/src/shop/WardrobePage.jsx
@@ -3,11 +3,12 @@ import { useState } from 'react';
 import './ShopPage.css'
 import AthenaHoodieHanger from '../assets/clothes/athenajumperonhanger.png'
 import RailImage from '../assets/rail.png'
+import AthenaHoodie from '../assets/clothes/athenajumper.png'
 
 const ClothesList = [
-    {image: AthenaHoodieHanger, title: 'athena award hoodie 1'},
-    {image: AthenaHoodieHanger, title: "npc safia's dress"},
-    {image: AthenaHoodieHanger, title: 'athena award hoodie 2'}
+    {image: AthenaHoodieHanger, title: 'athena award hoodie 1', openimage: AthenaHoodie},
+    {image: AthenaHoodieHanger, title: "npc safia's dress", openimage: AthenaHoodie},
+    {image: AthenaHoodieHanger, title: 'athena award hoodie 2', openimage: AthenaHoodie}
 ]
 
 // rail
@@ -25,18 +26,21 @@ function Rail() {
 function Clothes() {
     const [WhatClothesIsOpen, setWhatClothesIsOpen] = useState(null)
 
-    function OpenClothes() {
-        console.log('clothing rn')
-    }
-
     // we love .map(), will probably use that function here too
     // how do we get the image to stay in the background? idk
     return (
         <div id='Clothes'>
             {ClothesList.map((clothe, i) => (
-                <button className='Clothe' key={i} onClick={() => console.log(i)}>
-                    <img className='ClotheImage' src={clothe.image} alt={clothe.title} />
-                </button>
+                <div className='ClotheWrapper' key={i}>
+                    <button className='Clothe' key={i} onClick={() => setWhatClothesIsOpen(WhatClothesIsOpen === i ? null : i)}>
+                        <img className='ClotheImage' src={clothe.image} alt={clothe.title} />
+                    </button>
+                    {WhatClothesIsOpen === i && (
+                        <div className='OpenClothe' style={{backgroundImage: `url(${clothe.openimage})`}}>
+                            <p>hello! this is a project</p>
+                        </div>
+                    )}
+                </div>
             ))}
         </div>
     )
